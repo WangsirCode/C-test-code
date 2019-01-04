7b2803dcbcd7df49ce2a73b274ea08bbe9a12a84
bryant.wang <bryant.wang@dji.com>
2019-01-04 09:57:37 +0800

    return type does not overload

---
+++ overload.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+string GetValue(int value)
+{
+    return string("string");
+}
+
+int GetValue(int value)
+{
+    return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
