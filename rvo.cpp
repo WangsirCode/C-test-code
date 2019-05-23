9d80c549f7cbd25b18514704b11aa39aabfff3cc
bryant.wang <bryant.wang@dji.com>
2019-05-23 09:08:35 +0800

    add init

--- rvo.cpp
+++ rvo.cpp
@@ -57,8 +57,23 @@ A test3(bool condition)
     }
 }
 
+A test4()
+{
+    A tmp(4);
+    return std::move(tmp);
+}
+
+A&& test5()
+{
+    A tmp(5);
+    return std::move(tmp);
+}
+
 int main()
 {
+    cout << "test0" << endl;
+    A a0 = A();
+
     cout << "test1" << endl;
     A a ;
     a = test1();
@@ -72,11 +87,23 @@ int main()
     A c;
     c = test3(1);
     A d = test3(0);
+
+    cout << endl;
+    cout << "test4" << endl;
+    A e;
+    e = test4();
+
+    cout << endl;
+    cout << "test5" << endl;
+    A f;
+    f = test5();
+    A f1 = test5();
     return 0;
 }
 // 1. 对于非RVO， 对象会有两次拷贝，一次从被调用函数拷贝到临时区， 一次从临时区拷贝到调用函数
 // 2. 对于RVO，有两种情况， 1. A a = test(), 这种情况test里面直接操作a 的地址 2. A a; a = test(); 这种情况test 里面直接操作临时区的地址
 // 3. 如果函数里面有分支语句，不会触发RVO
+// 4. 对于A a = A(); 会先调用一次A的构造函数，然后Move一次； 和A a = test() 的区别在于， A()没有返回值， 不会有一次临时区的拷贝；
 
 // RVO 
 // g++ rvo.cpp -std=c++11
